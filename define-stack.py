8bd2f205aa5c14243e2361e071b15e0c3fadacaf
robfairh <ref3@illinois.edu>
2020-09-22 11:25:25 -0500

    pep8 issues

--- define-stack.py
+++ define-stack.py
@@ -1,19 +1,24 @@
 import numpy as np
 
-R = 48
-H = 2*R
 
-h = H/60
-print(h)
-stack = h*np.arange(60)
-stack = np.round(stack, 3)
+def writes_stack_lines():
+    '''
+    This file prints in the file 'untitled'
+    the necessary lines for defining a vertical stack
+    in serpent.
 
-f = open("untitled", "w+")
-for i in range(len(stack)):
-    if i % 10 == 0:
-        f.write('\n')
-    f.write(' '+str(stack[i])+' 4')
-f.close()
+    '''
+    R = 48
+    H = 2*R
 
-# R = 48
-# print(R/4)
+    h = H/60
+    print(h)
+    stack = h*np.arange(60)
+    stack = np.round(stack, 3)
+
+    f = open("untitled", "w+")
+    for i in range(len(stack)):
+        if i % 10 == 0:
+            f.write('\n')
+        f.write(' '+str(stack[i])+' 4')
+    f.close()
